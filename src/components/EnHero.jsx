2dc01bc495b576219195a518461233cdf373938f
marcelo-zuza <falecomzuza@gmail.com>
2023-08-29 15:09:53 -0300

    fixing mobile hero layout2

--- src/components/EnHero.jsx
+++ src/components/EnHero.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const EnHero = () => {
   return (
     <div>
-        <div className="bg-black w-full h-full pt-10 pb-28 px-2 md:p-18 md:py-16 font-BebasNeue tracking-widest">
+        <div className="bg-black w-full h-full pt-7 pb-28 px-2 md:p-18 md:py-16 font-BebasNeue tracking-widest">
             <div className="p-4 justify-center mb-2 md:mb-0">
                 <h2 className="text-white text-3xl md:text-5xl text-center">Full-Stack Web Developer</h2>
                 <h1 className="text-white text-6xl md:text-9xl -mb-1 md:-mb-4 text-center font-extrabold">MARCELO ZUZA's</h1>
@@ -15,7 +15,7 @@ const EnHero = () => {
                         <img src={Icone} alt="icone" className="h-60 md:h-60 pl-6" />
                 </div>
                 <div className="flex justify-center py-2 pb-20">
-                    <a href="/en/#about" className="bg-green-500 text-white text-3xl px-8 py-4 rounded-full hover:bg-white hover:text-green-500 duration-700 hover:px-14">Start Here</a>
+                    <a href="#about" className="bg-green-500 text-white text-3xl px-8 py-4 rounded-full hover:bg-white hover:text-green-500 duration-700 hover:px-14">Start Here</a>
                 </div>
             </div>
           </div>  
